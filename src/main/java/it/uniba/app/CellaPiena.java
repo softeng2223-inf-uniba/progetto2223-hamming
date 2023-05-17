7552e2751878d83ddf4b08f8ee9c73bae34aa9a9
Linonia <d.gentile32@studenti.uniba.it>
2023-05-17 16:16:46 +0200

    Creazione della classe CellaPiena

---
+++ src/main/java/it/uniba/app/CellaPiena.java
@@ -0,0 +1,28 @@
+package it.uniba.app;
+
+/**
+ * Classe che rappresenta una cella della griglia di gioco
+ * in cui è presente una nave.
+ * @author Gruppo Hamming
+ */
+public class CellaPiena extends Cella {
+    private Nave nave;
+
+    /**
+     * Costruttore della classe che inizializza la cella come non colpita e
+     * imposta la nave che occupa la cella.
+     * @param naveParam la nave che occupa la cella.
+     */
+    public CellaPiena(final Nave naveParam) {
+        super();
+        this.nave = naveParam;
+    }
+
+    /**
+     * Metodo che restituisce la nave che occupa la cella.
+     * @return la nave che occupa la cella.
+     */
+    public Nave getNave() {
+        return nave;
+    }
+}
