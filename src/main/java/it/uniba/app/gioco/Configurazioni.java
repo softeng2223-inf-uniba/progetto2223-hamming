f243d49658da0cf02c3236c7e9b95e5ab16c91a4
SimoneColumpsi <95437393+SimoneColumpsi@users.noreply.github.com>
2023-05-18 18:26:37 +0200

    Creazione della map CONFIGURAZIONE_LIVELLO contenente per ogni livello il numero di tentativi errati massimi

--- src/main/java/it/uniba/app/gioco/Configurazioni.java
+++ src/main/java/it/uniba/app/gioco/Configurazioni.java
@@ -1,4 +1,7 @@
 package it.uniba.app.gioco;
+import it.uniba.app.exceptions.LivelloNonEsistenteException;
+
+import java.util.Map;
 
 /**
  * Classe che contiene le configurazioni del gioco.
@@ -20,4 +23,22 @@ public final class Configurazioni {
         return COLONNE_GRIGLIA;
     }
 
+
+    // sezione livelli
+    private  static final Map<String, Integer> CONFIGURAZIONE_LIVELLO = Map.of(
+      "facile", 50,
+      "medio", 30,
+      "difficile", 10);
+
+    /**
+     * Restituisce il numero di tentativi di un livello di difficoltà.
+     *
+     * @param livello livello di difficoltà
+     */
+     public  static  int getTentativi(final  String livello) {
+         if (!CONFIGURAZIONE_LIVELLO.containsKey(livello)) {
+         throw new LivelloNonEsistenteException("Livello " + livello + " non esiste");
+         }
+         return CONFIGURAZIONE_LIVELLO.get(livello);
+     }
 }
