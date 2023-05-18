1ef36a65619bd30c51b3ada1b05f485c8e37df04
Marzia <m.capuano24@studenti.uniba.it>
2023-05-18 15:44:58 +0200

    Creazione ComandoNonEsistenteException

---
+++ src/main/java/it/uniba/app/exceptions/ComandoNonEsistenteException.java
@@ -0,0 +1,23 @@
+package it.uniba.app.exceptions;
+
+/**
+ * Eccezione lanciata quando si cerca di eseguire un comando non esistente.
+ * @author Gruppo Hamming
+ */
+public class ComandoNonEsistenteException extends Exception {
+    /**
+     * Costruttore senza parametri.
+     */
+    public ComandoNonEsistenteException() {
+        super();
+    }
+
+    /**
+     * Costruttore con parametro stringa.
+     * @param message messaggio da visualizzare
+     */
+    public ComandoNonEsistenteException(final String message) {
+        // ConfigurazioniInterfaccia.getComando(Util.suggestCommand(message)).getNome()
+        super("Il comando /" + message + " non esiste.");
+    }
+}
