1921984ff7fc1ac213a43eb8c527b9c2d7a40201
val-2 <programming.recantingly@aleeas.com>
2023-05-18 16:43:26 +0200

    Spostamento sezione comandi da Configurazioni a ConfigurazioniInterfaccia

--- src/main/java/it/uniba/app/gioco/Configurazioni.java
+++ src/main/java/it/uniba/app/gioco/Configurazioni.java
@@ -26,14 +26,4 @@ public final class Configurazioni {
         return COLONNE_GRIGLIA;
     }
 
-    //SEZIONE COMANDI
-    private static final Map<String, Comando> COMANDI = new LinkedHashMap<String, Comando>() { };
-
-    /**
-     * Metodo che restituisce l'oggetto comando associato alla stringa (chiave) passata come parametro.
-     * @param comando stringa che rappresenta il comando (chiave della LinkedHashMap)
-     */
-    public static Comando getComando(final String comando) {
-        return COMANDI.get(comando);
-    }
 }
--- src/main/java/it/uniba/app/interfaccia/ConfigurazioniInterfaccia.java
+++ src/main/java/it/uniba/app/interfaccia/ConfigurazioniInterfaccia.java
@@ -1,4 +1,24 @@
 package it.uniba.app.interfaccia;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class ConfigurazioniInterfaccia {
+
+    private ConfigurazioniInterfaccia() {
+    }
+
+    //SEZIONE COMANDI
+    private static final Map<String, Comando> COMANDI = new LinkedHashMap<String, Comando>() {
+    };
+
+    /**
+     * Metodo che restituisce l'oggetto comando associato alla stringa (chiave) passata come parametro.
+     *
+     * @param comando stringa che rappresenta il comando (chiave della LinkedHashMap)
+     */
+    public static Comando getComando(final String comando) {
+        return COMANDI.get(comando);
+    }
+
 }
