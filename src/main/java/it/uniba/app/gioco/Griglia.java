8f82524b411c874d8f29826d24aab554e1c4fca5
Fabb-24 <cirullofabio24@gmail.com>
2023-05-19 08:28:17 +0200

    Aggiunta del metodo posizionaNave nella classe Griglia

--- src/main/java/it/uniba/app/gioco/Griglia.java
+++ src/main/java/it/uniba/app/gioco/Griglia.java
@@ -41,6 +41,43 @@ public class Griglia implements Cloneable {
     }
 
     /**
+        Metodo che posiziona una nave sulla griglia.
+        @return true se la nave è stata posizionata, false altrimenti
+        @param nave nave da posizionare
+        @param r riga della prima cella che la nave deve occupare
+        @param c colonna della prima cella che la nave deve occupare
+        @param orizzontale true se la nave è orizzontale, false se è verticale
+    */
+    public boolean posizionaNave(final Nave nave, final int r, final int c, final boolean orizzontale) {
+        if (orizzontale) {
+            if (c + nave.getLunghezza() > Configurazioni.getColonneGriglia()) {
+                return false;
+            }
+            for (int i = c; i < c + nave.getLunghezza(); i++) {
+                if (!getCella(r, i).eVuota()) {
+                    return false;
+                }
+            }
+            for (int y = c; y < c + nave.getLunghezza(); y++) {
+                celle[r][y] = new CellaPiena(nave);
+            }
+        } else {
+            if (r + nave.getLunghezza() > Configurazioni.getRigheGriglia()) {
+                return false;
+            }
+            for (int i = r; i < r + nave.getLunghezza(); i++) {
+                if (!getCella(i, c).eVuota()) {
+                    return false;
+                }
+            }
+            for (int y = r; y < r + nave.getLunghezza(); y++) {
+                celle[y][c] = new CellaPiena(nave);
+            }
+        }
+        return true;
+    }
+
+    /**
      * Metodo che clona la griglia.
      * @return la griglia clonata.
      */
