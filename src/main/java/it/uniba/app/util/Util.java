306166c847a580bc269e18c33e36387de03dbc4e
Linonia <d.gentile32@studenti.uniba.it>
2023-05-19 12:34:06 +0200

    Aggiunta della funzione che calcola la distanza di Levenshtein

--- src/main/java/it/uniba/app/util/Util.java
+++ src/main/java/it/uniba/app/util/Util.java
@@ -13,4 +13,33 @@ public final class Util {
     public static String getString() {
         return scan.nextLine();
     }
+
+
+    /**
+     * Metodo che calcola la distanza di Levenshtein tra due stringhe.
+     */
+    public static int getLevenshteinDistance(final String s, final String t) {
+        int[][] d = new int[s.length() + 1][t.length() + 1];
+        for (int i = 0; i <= s.length(); i++) {
+            d[i][0] = i;
+        }
+
+        for (int j = 0; j <= t.length(); j++) {
+            d[0][j] = j;
+        }
+        for (int j = 1; j <= t.length(); j++) {
+            for (int i = 1; i <= s.length(); i++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    d[i][j] = d[i - 1][j - 1];
+                } else {
+                    int substitutionCost = d[i - 1][j - 1] + 1;
+                    int insertionCost = d[i][j - 1] + 1;
+                    int deletionCost = d[i - 1][j] + 1;
+                    int minCost = Math.min(substitutionCost, Math.min(insertionCost, deletionCost));
+                    d[i][j] = minCost;
+                }
+            }
+        }
+        return d[s.length()][t.length()];
+    }
 }
