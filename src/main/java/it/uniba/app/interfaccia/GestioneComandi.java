125cbe87e256c1429ae781358420e5af6562eb84
Linonia <d.gentile32@studenti.uniba.it>
2023-05-19 12:08:25 +0200

    Spostamento della classe MostraNavi fuori dalla classe Gioca perché posizionata male nel file in precedenza

--- src/main/java/it/uniba/app/interfaccia/GestioneComandi.java
+++ src/main/java/it/uniba/app/interfaccia/GestioneComandi.java
@@ -235,18 +235,18 @@ class Gioca extends Comando {
             System.out.println("Clone non supportato");
         }
     }
+}
 
-    class MostraNavi extends Comando {
-        MostraNavi() {
-            super("mostraNavi", "utility");
-        }
+class MostraNavi extends Comando {
+    MostraNavi() {
+        super("mostraNavi", "utility");
+    }
 
-        String getDescrizione() {
-            return "Mostra le navi presenti nella griglia";
-        }
+    String getDescrizione() {
+        return "Mostra le navi presenti nella griglia";
+    }
 
-        void esegui() {
-            Grafica.stampaNavi();
-        }
+    void esegui() {
+        Grafica.stampaNavi();
     }
 }
