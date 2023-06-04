0ebc1e0cb11fc341234527e9ece342f2bab67c5a
val-2 <programming.recantingly@aleeas.com>
2023-06-04 18:48:57 +0200

    Correzione warning checkstyle

--- src/main/java/it/uniba/app/interfaccia/ConfigurazioniInterfaccia.java
+++ src/main/java/it/uniba/app/interfaccia/ConfigurazioniInterfaccia.java
@@ -55,11 +55,11 @@ public final class ConfigurazioniInterfaccia {
     public static final String ANSI_GREEN = "\u001B[32m";
     public static final String ANSI_CYAN = "\u001B[36m";
     public static final String ANSI_MAGENTA = "\u001B[35m";
-    public static final String SIMBOLO_DEFAULT = "X";//"\u25A0";
+    public static final String SIMBOLO_DEFAULT = "X"; //"■";
 
     /**
      * Restituisce il simbolo di default delle navi.
-     * 
+     *
      * @return simbolo di default delle navi
      */
     public static String getSimboloDefault() {
