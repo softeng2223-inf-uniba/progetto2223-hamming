2a11ef549b05204ed040cfe74ae11f5c290e81f0
SimoneColumpsi <simcolu@gmail.com>
2023-06-05 19:04:37 +0200

    Aggiunta gestione eccezione a catch block

--- src/main/java/it/uniba/app/App.java
+++ src/main/java/it/uniba/app/App.java
@@ -1,6 +1,7 @@
 package it.uniba.app;
 
 import it.uniba.app.exceptions.ComandoNonEsistenteException;
+import it.uniba.app.exceptions.InputNonFormattatoException;
 import it.uniba.app.interfaccia.GestioneComandi;
 
 /**
@@ -26,7 +27,7 @@ public final class App {
         if (args.length == 1 && ("--help".equals(args[0]) || "-h".equals(args[0]))) {
             try {
                 GestioneComandi.chiamaComando("/help", new String[0]);
-            } catch (ComandoNonEsistenteException e) {
+            } catch (ComandoNonEsistenteException | InputNonFormattatoException e) {
                 System.out.println(e.getMessage());
             }
         }
