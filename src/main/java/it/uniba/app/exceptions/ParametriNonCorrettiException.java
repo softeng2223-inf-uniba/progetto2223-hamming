25b50ebfeaada1bcd4c4344db877e93b6cd3a762
Linonia <d.gentile32@studenti.uniba.it>
2023-06-08 15:02:06 +0200

    Risolti problemi checkstyle in ParametriNonCorrettiException

--- src/main/java/it/uniba/app/exceptions/ParametriNonCorrettiException.java
+++ src/main/java/it/uniba/app/exceptions/ParametriNonCorrettiException.java
@@ -1,11 +1,23 @@
 package it.uniba.app.exceptions;
 
+/**
+ * Eccezione lanciata quando i parametri passati al comando non sono corretti.
+ *
+ * @author Gruppo Hamming
+ */
 public class ParametriNonCorrettiException extends Exception {
-    
+
+    /**
+     * Costruttore con parametri. Chiama il costruttore della superclasse e passa il messaggio di errore.
+     * @param message Messaggio da mostrare all'utente.
+     */
     public ParametriNonCorrettiException(final String message) {
         super(message);
     }
 
+    /**
+     * Costruttore di default. Chiama il costruttore della superclasse.
+     */
     public ParametriNonCorrettiException() {
         super("Parametri non corretti per il comando.");
     }
