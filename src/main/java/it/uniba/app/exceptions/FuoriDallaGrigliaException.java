d9046786d1d93e2ff771042789158409fec56769
Marzia <m.capuano24@studenti.uniba.it>
2023-06-11 15:55:42 +0200

    "Creati costruttori dell'eccezione"

--- src/main/java/it/uniba/app/exceptions/FuoriDallaGrigliaException.java
+++ src/main/java/it/uniba/app/exceptions/FuoriDallaGrigliaException.java
@@ -5,6 +5,23 @@ package it.uniba.app.exceptions;
  * @author Gruppo Hamming
  */
 public class FuoriDallaGrigliaException extends Exception {
+    /**
+     * Costruttore senza parametri.
+     */
+    public FuoriDallaGrigliaException() {
+        super("Coordinate errate, sono fuori dalla griglia.");
+    }
+
+    /**
+     * Costruttore con parametro stringa.
+     *
+     * @param riga   numero righe
+     * @param colonna numero colonne
+     */
+    public FuoriDallaGrigliaException(final int riga, final int colonna) {
+        super("Le coordinate " + ((char) (colonna + 'a')) + "-" + (String.valueOf(riga + 1))
+                + " inserite sono fuori dalla griglia");
+    }
 
 }
 
