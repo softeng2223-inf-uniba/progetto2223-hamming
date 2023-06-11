ebcc3e35fd4507b8b88a931b192294270f799fa0
Marzia <m.capuano24@studenti.uniba.it>
2023-06-11 16:02:23 +0200

    "Creazione dei costruttori nell'eccezione CellaGiaColpitaException"

--- src/main/java/it/uniba/app/exceptions/CellaGiaColpitaException.java
+++ src/main/java/it/uniba/app/exceptions/CellaGiaColpitaException.java
@@ -1,5 +1,25 @@
 package it.uniba.app.exceptions;
 
-public class CellaGiaColpitaException extends Exception{
+/**
+ * Eccezione lanciata quando viene attaccata una cella già colpita in precedenza.
+ *
+ * @author Gruppo Hamming
+ */
+public class CellaGiaColpitaException extends Exception {
+    /**
+     * Costruttore senza parametri.
+     */
+    public CellaGiaColpitaException() {
+        super("La cella è già colpita.");
+    }
 
+    /**
+     * Costruttore con parametro stringa.
+     *
+     * @param riga numero righe
+     */
+    public CellaGiaColpitaException(final int riga, final int colonna) {
+        super("La cella delle coordinate " + ((char) (colonna + 'a')) + "-" + (String.valueOf(riga + 1))
+                + " è stata già colpita");
+    }
 }
