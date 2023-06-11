279288674fd84d70a78461a86ff1dd1f79ea6101
Marzia <m.capuano24@studenti.uniba.it>
2023-06-11 15:44:43 +0200

    "Creazione eccezione FuoriDallaGrigliaException"

---
+++ src/main/java/it/uniba/app/exceptions/FuoriDallaGrigliaException.java
@@ -0,0 +1,10 @@
+package it.uniba.app.exceptions;
+/**
+ * Eccezione lanciata quando vengono inserite coordinate errate.
+ *
+ * @author Gruppo Hamming
+ */
+public class FuoriDallaGrigliaException extends Exception {
+
+}
+
