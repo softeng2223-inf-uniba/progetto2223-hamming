9afaeebdfd200edc50f1376a2cd9e9ae97db4cb6
Baio02 <cirullofabio24@gmail.com>
2023-06-12 16:50:27 +0200

    Creazione enumerazione per l'esito di un colpo

---
+++ src/main/java/it/uniba/app/gioco/EsitoColpo.java
@@ -0,0 +1,8 @@
+package it.uniba.app.gioco;
+
+/**
+ * Enumerazione che rappresenta l'esito di un colpo sparato sulla griglia di gioco.
+ */
+public enum EsitoColpo {
+    ACQUA, COLPITO, AFFONDATO
+}
