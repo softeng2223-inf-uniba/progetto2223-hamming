764d6ca58eb331c192f4e2a192e7cf0dc739abd4
Baio02 <cirullofabio24@gmail.com>
2023-06-13 10:09:54 +0200

    Creazione della classe control GestioneStampe

---
+++ src/main/java/it/uniba/app/interfaccia/GestioneStampe.java
@@ -0,0 +1,14 @@
+package it.uniba.app.interfaccia;
+
+/**
+ * <<Control>>
+ * Classe che gestisce le stampe a schermo.
+ * Si occupa delle stampe che riguardano la griglia di gioco.
+ *
+ * @author Gruppo Hamming
+ */
+public final class GestioneStampe {
+
+    private GestioneStampe() {
+    }
+}
