60435de2ca9a08882b38203446bd4e05757cdd4b
Baio02 <cirullofabio24@gmail.com>
2023-06-13 10:44:46 +0200

    aggiunto il tag @author al javadoc di EsitoColpo

--- src/main/java/it/uniba/app/gioco/EsitoColpo.java
+++ src/main/java/it/uniba/app/gioco/EsitoColpo.java
@@ -2,6 +2,8 @@ package it.uniba.app.gioco;
 
 /**
  * Enumerazione che rappresenta l'esito di un colpo sparato sulla griglia di gioco.
+ *
+ * @author Gruppo Hamming
  */
 public enum EsitoColpo {
     ACQUA, COLPITO, AFFONDATO
