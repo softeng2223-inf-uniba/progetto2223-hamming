f34cc7d797f7d412febe64c74fed8eb313f81d67
val-2 <programming.recantingly@aleeas.com>
2023-06-14 10:21:08 +0200

    Uso di classe per manipolare stdin e stdout

--- src/test/java/it/uniba/app/interfaccia/GestioneStampeTest.java
+++ src/test/java/it/uniba/app/interfaccia/GestioneStampeTest.java
@@ -2,15 +2,9 @@ package it.uniba.app.interfaccia;
 
 import it.uniba.app.gioco.Griglia;
 import it.uniba.app.gioco.Nave;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import it.uniba.app.util.SimulaStdIOStream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import java.nio.charset.StandardCharsets;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -19,40 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * Classe di test per la classe GestioneStampe.
  * {@link GestioneStampe}
  */
-class GestioneStampeTest {
-    private static ByteArrayOutputStream outputStream;
-    private static PrintStream stdOut;
-
-    /**
-     * Metodo eseguito una volta all'inizio del test.
-     * Serve per poter intercettare lo standard output
-     * reindirizzandolo su outputStream.
-     */
-    @BeforeAll
-    static void setUp() {
-        stdOut = System.out;
-        outputStream = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outputStream, true, StandardCharsets.UTF_8));
-    }
-
-    /**
-     * Resetta outputStream prima di ogni test,
-     * in modo da cancellare l'output dei test precedenti.
-     */
-    @BeforeEach
-    void setUpEach() {
-        outputStream.reset();
-    }
-
-    /**
-     * Ripristina lo standard output.
-     */
-    @AfterAll
-    static void tearDown() {
-        System.setOut(stdOut);
-    }
-
-
+class GestioneStampeTest extends SimulaStdIOStream {
     /**
      * Test di stampaGrigliaColpita.
      * Il test intercetta lo standard output e verifica che sia stata
@@ -89,7 +50,7 @@ class GestioneStampeTest {
                 """;
         Griglia griglia = new Griglia();
         GestioneStampe.stampaGrigliaColpita(griglia);
-        String actualOutput = outputStream.toString(StandardCharsets.UTF_8);
+        String actualOutput = leggiOutput();
         actualOutput = actualOutput.replaceAll("\r", ""); // rende compatibile il test con Windows
         assertEquals(expectedOutput, actualOutput, "La griglia vuota non è stampata correttamente");
     }
@@ -141,7 +102,7 @@ class GestioneStampeTest {
         griglia.posizionaNave(nave1, r1, c1, true);
         griglia.posizionaNave(nave2, r2, c2, false);
         GestioneStampe.svelaGrigliaNavi(griglia);
-        String actualOutput = outputStream.toString(StandardCharsets.UTF_8);
+        String actualOutput = leggiOutput();
         actualOutput = actualOutput.replaceAll("\r", ""); // rende compatibile il test con Windows
         actualOutput = actualOutput.replaceAll("\\u001B\\[\\d{1,2}m", ""); // rimuove i colori
         assertEquals(expectedOutput, actualOutput, "La griglia vuota non è stampata correttamente");
