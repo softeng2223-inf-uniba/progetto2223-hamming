bd3748a4febb9bfd314a282ece92c721a16b11d6
val-2 <programming.recantingly@aleeas.com>
2023-06-14 10:25:54 +0200

    Creazione test lettura attacco

--- src/test/java/it/uniba/app/AppTest.java
+++ src/test/java/it/uniba/app/AppTest.java
@@ -75,4 +75,55 @@ class AppTest extends SimulaStdIOStream {
                 "leggiInput ha riconosciuto un comando senza slash come ben formattato");
     }
 
+    /**
+     * Test di lettura di un attacco in leggiInput.
+     * {@link App#leggiInput()}
+     */
+    @Test
+    @DisplayName("leggiInput accetta un attacco valido (A-3)")
+    void testLeggiInputAttacco() {
+        String input = "A-3";
+        simulaInput(input);
+        String inputLetto;
+        try {
+            inputLetto = App.leggiInput();
+        } catch (InputNonFormattatoException e) {
+            fail("leggiInput ha riconosciuto un attacco valido come ben formattato");
+            return;
+        }
+        assertEquals(input.toLowerCase(), inputLetto, "L'attacco letto non è uguale a quello inserito");
+    }
+
+    /**
+     * Test di lettura di un attacco con riga e colonna per taglie
+     * di griglia grandi in leggiInput.
+     * {@link App#leggiInput()}
+     */
+    @Test
+    @DisplayName("leggiInput accetta un attacco valido per taglia di griglia extralarge (Z-26)")
+    void testLeggiInputAttaccoGrande() {
+        String input = "Z-26";
+        simulaInput(input);
+        String inputLetto;
+        try {
+            inputLetto = App.leggiInput();
+        } catch (InputNonFormattatoException e) {
+            fail("leggiInput ha riconosciuto un attacco valido come mal formattato");
+            return;
+        }
+        assertEquals(input.toLowerCase(), inputLetto, "L'attacco letto non è uguale a quello inserito");
+    }
+
+    /**
+     * Test di lettura di un attacco non valido in leggiInput.
+     * {@link App#leggiInput()}
+     */
+    @Test
+    @DisplayName("leggiInput non accetta un attacco non valido (A-555)")
+    void testLeggiInputAttaccoNonValido() {
+        String input = "A-555";
+        simulaInput(input);
+        assertThrows(InputNonFormattatoException.class, App::leggiInput,
+                "leggiInput ha accettato un attacco non valido");
+    }
 }
